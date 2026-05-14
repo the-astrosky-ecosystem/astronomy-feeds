d851eb510818ee976428867079eb4fa1bbba7a27
Vincent Carpenter <vincent.s.carpenter@gmail.com>
2026-05-13 17:43:41 -0700

    dropping in actual obfuscation method

--- scripts/sql/devdb/populate_dev_db.sql
+++ scripts/sql/devdb/populate_dev_db.sql
@@ -42,14 +42,56 @@ SELECT * FROM (
     LIMIT 50000
 )
 ORDER BY request_dt ASC;
--- anonymize the DID of users viewing the feed (request_user_did field) here!
-/*
-the below code works for simple replacement, but we need to maintain the uniqueness 
-of each did.
-*/
---UPDATE public.activitylog
---SET request_user_did = 'did:plc:a'
---WHERE request_user_did LIKE 'did:plc:%';
+
+/* anonymize user DIDs in activity log
+
+start by gathering all unique user DIDs from activity log into a temporary table */
+DROP TABLE IF EXISTS obfuscation_map;
+CREATE TEMPORARY TABLE obfuscation_map ON COMMIT DROP
+AS
+SELECT DISTINCT ON (request_user_did) request_user_did
+FROM public.activitylog
+WHERE request_user_did != 'Unknown';
+
+/* add a column for corresponding obfuscated did */
+ALTER TABLE obfuscation_map
+ADD COLUMN obfuscated_user_did varchar(255) UNIQUE;
+
+/* fill obfuscated DID column by prepending appropriate DID identifiers to random UUIDs */
+UPDATE obfuscation_map
+SET obfuscated_user_did = 'did:plc:' || gen_random_uuid()
+WHERE request_user_did LIKE 'did:plc:%';
+
+UPDATE obfuscation_map
+SET obfuscated_user_did = 'did:web:' || gen_random_uuid()
+WHERE request_user_did LIKE 'did:web:%';
+
+/* overwrite original DIDs in table with obfuscated DIDs (possibly is a way to do this without scripting, but I didn't find it) */
+CREATE OR REPLACE FUNCTION obfuscate() RETURNS integer AS $$
+DECLARE
+	obfuscation_map_row obfuscation_map%ROWTYPE;
+	did obfuscation_map.request_user_did%TYPE;
+	obfuscated_did obfuscation_map.obfuscated_user_did%TYPE;
+BEGIN
+	RAISE NOTICE 'Entering obfuscation function.';
+
+	FOR obfuscation_map_row IN
+	SELECT * FROM obfuscation_map
+	ORDER BY 1
+	LOOP
+		did = obfuscation_map_row.request_user_did;
+		obfuscated_did = obfuscation_map_row.obfuscated_user_did;
+		RAISE NOTICE 'replacing user DID % with obfuscated DID %', quote_ident(did), quote_ident(obfuscated_did);
+		UPDATE public.activitylog
+		SET request_user_did = obfuscated_did
+		WHERE request_user_did = did;
+	END LOOP;
+
+	RETURN 1;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT obfuscate();
 
 -- not taking NormalizedFeedStats for now - maybe generate after the fact?
 
